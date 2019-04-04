9cc384837aefecfd96fcfea67ae8e60842efa597
BlindedShooter <gkadnflh@gmail.com>
2019-04-04 13:15:13 +0900

    Reformatted Code

--- models/LinearRegression.py
+++ models/LinearRegression.py
@@ -1,19 +1,19 @@
 import numpy as np
 
+
 class LinearRegression:
     def __init__(self, num_features):
         self.num_features = num_features
         self.W = np.zeros((self.num_features, 1))
 
     def train(self, x, y, epochs, batch_size, lr, optim):
-        final_loss = None   # loss of final epoch
+        final_loss = None  # loss of final epoch
 
         # Training should be done for 'epochs' times with minibatch size of 'batch_size'
         # The function 'train' should return the loss of final epoch
         # Loss of an epoch is calculated as an average of minibatch losses
 
         # ========================= EDIT HERE ========================
-        self.W = np.ones((self.num_features, 1))
         # Determining the number of mini batches
         batches = x.shape[0] // batch_size
         if x.shape[0] % batch_size:
@@ -22,8 +22,8 @@ class LinearRegression:
         for i in range(epochs):
             for j in range(batches):
                 # Separating batch data for ease of implementation
-                batch_x = x[j*batch_size:(j + 1)*batch_size]
-                batch_y = y[j*batch_size:(j + 1)*batch_size]
+                batch_x = x[j * batch_size:(j + 1) * batch_size]
+                batch_y = y[j * batch_size:(j + 1) * batch_size]
 
                 # n is the batch size, reason for not using the batch_size : at the end of the epoch, leftover data
                 # size become smaller than the the batch_size
